e5d197b3baf5d0f52c740992ed91962ef52b3e4f
ReekyGiraffe <20372442@live.blackburn.ac.uk>
2026-04-14 14:07:26 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function showDateTime () {
+    document.getElementById("datetime").innerHTML =
+     "Date & Time: " + new Date();
+}
+
+function validateForm() {
+    if (
+        document.getElementById("name").value == "" ||
+        document.getElementById("email").value == "" ||
+        document.getElementById("message").value == ""
+    ) {
+        alert("Please fill all fields");
+        return false;
+    }
+    alert("Form submitted successfully");
+    return true;
+}
